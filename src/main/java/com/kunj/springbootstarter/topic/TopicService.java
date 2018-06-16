1cea5ffa1de84b0297671ad1217b204b8cd21b1d
kunj1755 <kunj1755@192.168.2.4>
2018-06-16 15:38:38 +0530

    Creating a new resource using post

--- src/main/java/com/kunj/springbootstarter/topic/TopicService.java
+++ src/main/java/com/kunj/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.kunj.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,16 +9,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description")				
-			);
+			));
+	
 	public List<Topic> getAllTopics(){
 		return topics;
 	}
 	
 	public Topic getTopic(String id) {
 		return (Topic) topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
